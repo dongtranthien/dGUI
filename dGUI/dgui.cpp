633ca4422c982d51fbcd964826e5b9533733d6bb
Đông Trần Thiên <thiendong.iuh@gmail.com>
2019-04-24 19:59:35 +0700

    Delete free labels pointer
    Error when free labels pointer which is empty

--- dGUI/dgui.cpp
+++ dGUI/dgui.cpp
@@ -29,38 +29,11 @@ void dGui::update() {
 void dGui::add(label &label_) {
 	if (labels_num == 0) {
 		labels_num++;
-		free(labels);
 
 		labels = (label**)calloc(labels_num, sizeof(label*));
 		labels[labels_num - 1] = &label_;
 	}
 	else {
-		//// Create label list temp to recalloc label list pointer
-		//label** labels_t = (label**)calloc(labels_num, sizeof(label*));
-		//// memccpy(labels_t, labels, NULL, labels_num * sizeof(label*));
-		//// Copy pointer label list to temp
-		//memcpy(labels_t, labels, labels_num * sizeof(label*));
-		///*uint16_t i;
-		//for (i = 0; i < labels_num; i++) {
-		//	labels_t[i] = labels[i];
-		//}*/
-		//// Deallocates of labels list pointer
-		//free(labels);
-		//// Increase labels num
-		//labels_num++;		
-		//// Create new list of label
-		//labels = (label**)calloc(labels_num, sizeof(label*));
-		//// Copy labels temp to label
-		//memcpy(labels, labels_t, labels_num * sizeof(label*));
-		///*for (i = 0; i < (labels_num - 1); i++) {
-		//	labels[i] = labels_t[i];
-		//}*/
-		//// Import new label pointer to labels list
-		//labels[labels_num - 1] = &label_;
-		//// Deallocates temp pointer list
-		//free(labels_t);
-		
-
 		// Increase labels num
 		labels_num++;
 
